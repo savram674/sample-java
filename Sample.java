039d42a7261e1e01507fe3f7ce0c076aefee770b
SAVITHA RAMESH <72896893+savram674@users.noreply.github.com>
2026-09-23 15:05:10 +0530

    greetings
    Removed user input and sum calculation for a simple greeting.

--- Sample.java
+++ Sample.java
@@ -1,19 +1,7 @@
-import java.util.Scanner;
-
 public class Sample {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int a = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int b = sc.nextInt();
-
-        int sum = a + b;
-
-        System.out.println("Sum = " + sum);
+     System.out.println("Hello Welcome to Jenkins");
 
         sc.close();
     }
